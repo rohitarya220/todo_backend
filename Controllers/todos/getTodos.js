50f276df12f02e5483395f99ce8200747998e123
Rohit kumar <rohitarya7478@gmail.com>
2024-07-08 21:23:09 +0530

    role based access, cors install

--- Controllers/todos/getTodos.js
+++ Controllers/todos/getTodos.js
@@ -1,20 +1,20 @@
 const todoModal = require("../../Model/todo");
 
-const getTodos = async(req, res) =>{
+const getTodos = async (req, res) => {
     let isRole = {}
-    const {userId} = req.body
+    const { userId } = req.body
     try {
 
-        if(req.body.role === "Admin"){
-            isRole={}
-        }else{
-            isRole = {userId}
+        if (req.body.role === "Admin") {
+            isRole = {}
+        } else {
+            isRole = { userId }
         }
         const todos = await todoModal.find(isRole)
 
-        res.status(200).json({data: todos})
+        res.status(200).json({ data: todos })
     } catch (error) {
-        res.status(500).json({message: error.message});
+        res.status(500).json({ message: error.message });
     }
 }
 
--- index.js
+++ index.js
@@ -3,9 +3,17 @@ require('dotenv').config()
 const connection = require('./config/db')
 const userRoutes = require('./Routers/users')
 const todoRoutes = require('./Routers/todos')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 8000 
 const app = express();
+
+app.use(cors({
+    origin: '*',
+    optionsSuccessStatus: 200,
+    // credentials: true,
+    methods: 'GET, POST, PUT, DELETE'
+}));
 
 
 app.use(express.json())
